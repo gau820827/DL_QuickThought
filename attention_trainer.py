48a4bbbe2b75712a940baf0bbd3dac42f1d18e7a
Cheng-Hsi Chao <chc631@nyu.edu>
2018-05-06 15:56:36 -0400

    Clean up old functions

--- attention_trainer.py
+++ attention_trainer.py
@@ -6,12 +6,12 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import random
 import importlib
+
 import data
 import utils
 import time
 import json
 import models
-
 
 
 # This is the iterator we'll use during training.
@@ -61,68 +61,6 @@ def get_batch(batch):
     return vectors, labels
 
 
-def evaluate(model, data_iter):
-    model.eval()
-    correct = 0
-    total = 0
-    for i in range(len(data_iter)):
-        vectors, labels = get_batch(data_iter[i])
-        vectors = Variable(torch.stack(vectors).squeeze())
-        labels = torch.stack(labels).squeeze()
-        output = model(vectors)
-        _, predicted = torch.max(output.data, 1)
-        total += labels.size(0)
-        if (predicted.shape != labels.shape):
-            break
-        correct += (predicted.cpu() == labels.cpu()).sum()
-        # correct += (predicted == labels).sum()
-    return correct / float(total)
-
-def training_loop(model, criterion, optimizer, training_iter, dev_iter, train_eval_iter, use_gpu=False):
-    step = 0
-    for i in range(num_train_steps):
-        model.train()
-        vectors, labels = get_batch(next(training_iter))
-        vectors = Variable(torch.stack(vectors).squeeze())
-        labels = Variable(torch.stack(labels).squeeze())
-        if (use_gpu):
-            vectors = vectors.cuda()
-            labels = labels.cuda()
-
-        model.zero_grad()
-        output = model(vectors)
-
-        loss = criterion(output, labels)
-        loss.backward(retain_graph=True)
-        # loss.backward()
-        optimizer.step()
-
-        if step % 100 == 0:
-            print( "Step %i; Loss %f; Train acc: %f; Dev acc %f"
-                %(step, loss.data[0], evaluate(model, train_eval_iter), evaluate(model, dev_iter)))
-
-        step += 1
-
-def train(model, use_gpu):
-    model = model
-
-    # Loss and Optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    # Train the model
-    training_iter = data_iter(dataset.training_set, batch_size)
-    train_eval_iter = eval_iter(dataset.training_set[0:500], batch_size)
-    dev_iter = eval_iter(dataset.dev_set[0:500], batch_size)
-    training_loop(model, criterion, optimizer, training_iter, dev_iter, train_eval_iter, use_gpu)
-    return model
-
-def test(model):
-    # Test the model
-    test_iter = eval_iter(dataset.test_set, batch_size)
-    test_acc = evaluate(model, test_iter)
-    print('Accuracy of the network on the test data: %f' % (100 * test_acc))
-
-
 # Set the random seed manually for reproducibility.
 # seed = 1111
 # torch.manual_seed(seed)
@@ -134,18 +72,24 @@ def test(model):
 # random.seed(seed)
 
 
-def training_loop2(model, criterion, optimizer, training_iter, dev_iter, train_eval_iter, step, total_pure_loss, total_loss, cuda=False):
+def training_loop(model, criterion, optimizer, training_iter, dev_iter, train_eval_iter, step, total_pure_loss, total_loss, cuda=False):
     for i in range(num_train_steps):
         model.train()
         data, targets = get_batch(next(training_iter))
         data = Variable(torch.stack(data).squeeze())
         targets = Variable(torch.stack(targets).squeeze())
-        if (cuda):
+        if cuda:
             data = data.cuda()
             targets = targets.cuda()
-        hidden = model.init_hidden(data.size(1))
-        output, attention = model.forward(data, hidden)
-        # print ("attention:", attention)
+
+        try:
+            hidden = model.init_hidden(data.size(1))
+            output, attention = model.forward(data, hidden)
+        except:
+            model.zero_grad()
+            output = model(data)
+            attention = None
+
         loss = criterion(output.view(data.size(1), -1), targets)
         total_pure_loss += loss.data
 
@@ -168,13 +112,9 @@ def training_loop2(model, criterion, optimizer, training_iter, dev_iter, train_e
             print(
                    "Step",  step,
                    "Loss ", loss.data[0],
-                   " Train loss/acc: ", str(evaluate2(model, train_eval_iter)),
-                   "Dev loss/acc: " + str(evaluate2(model, dev_iter))
+                   " Train loss/acc: ", str(evaluate(model, train_eval_iter)),
+                   "Dev loss/acc: " + str(evaluate(model, dev_iter))
                  )
-            # print( "Step",  step)
-            # print ("Loss ", loss.data[0])
-            # print (" Train loss/acc: ", str(evaluate2(model, train_eval_iter)))
-            # print ("Dev loss/acc: " + str(evaluate2(model, dev_iter)))
             # print('| {:5d}/{:5d} batches | ms/batch {:5.2f} | loss {:5.4f} | pure loss {:5.4f}'.format(
             #        i, len(data_train) // batch_size,
             #       elapsed * 1000 / log_interval, total_loss[0] / log_interval,
@@ -184,27 +124,15 @@ def training_loop2(model, criterion, optimizer, training_iter, dev_iter, train_e
             # start_time = time.time()
         step += 1
 
-        """
-        if i % log_interval == 0 and i > 0:
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | ms/batch {:5.2f} | loss {:5.4f} | pure loss {:5.4f}'.format(
-                  epoch_number, i, len(data_train) // batch_size,
-                  elapsed * 1000 / log_interval, total_loss[0] / log_interval,
-                  total_pure_loss[0] / log_interval))
-            total_loss = 0
-            total_pure_loss = 0
-            start_time = time.time()
-        """
-
 #            for item in model.parameters():
 #                print item.size(), torch.sum(item.data ** 2), torch.sum(item.grad ** 2).data[0]
 #            print model.encoder.ws2.weight.grad.data
 #            exit()
 
-def test2(model, epoch_number, evaluate_start_time):
+def test(model, epoch_number, evaluate_start_time):
     # Test the model
     test_iter = eval_iter(dataset.test_set, batch_size)
-    val_loss, test_acc = evaluate2(model, test_iter)
+    val_loss, test_acc = evaluate(model, test_iter)
     print('-' * 89)
     fmt = '| evaluation | epoch: {} |time: {:5.2f}s | valid loss (pure) {:5.4f} | Acc {:8.4f}'
     print(fmt.format(epoch_number, (time.time() - evaluate_start_time), val_loss, test_acc))
@@ -234,26 +162,7 @@ def save_model(model, val_loss, acc, epoch_number):
     f.close()
 
 
-def train2(model, epoch_number, cuda=True):
-    model.train()
-    total_loss = 0
-    total_pure_loss = 0  # without the penalization term
-    start_time = time.time()
-    step = 0
-
-
-    training_iter = data_iter(dataset.training_set, batch_size)
-    train_eval_iter = eval_iter(dataset.training_set[0:500], batch_size)
-    dev_iter = eval_iter(dataset.dev_set[0:500], batch_size)
-
-    training_loop2(model, criterion, optimizer, training_iter, dev_iter, train_eval_iter, step, total_pure_loss, total_loss, cuda)
-
-    evaluate_start_time = time.time()
-    val_loss, acc = test2(model, epoch_number, evaluate_start_time)
-    save_model(model, val_loss, acc, epoch_number)
-
-
-def evaluate2(model, data_iter):
+def evaluate(model, data_iter):
     """evaluate the model while training"""
     model.eval()  # turn on the eval() switch to disable dropout
     total_loss = 0
@@ -269,8 +178,13 @@ def evaluate2(model, data_iter):
         if cuda:
             data = data.cuda()
             targets = targets.cuda()
-        hidden = model.init_hidden(data.size(1))
-        output, attention = model.forward(data, hidden)
+
+        # model use Attention:
+        try:
+            hidden = model.init_hidden(data.size(1))
+            output, attention = model(data, hidden)
+        except:
+            output = model(data)
         output_flat = output.view(data.size(1), -1)
         curr_loss = criterion(output_flat, targets)
         total_loss += curr_loss.data
@@ -278,6 +192,25 @@ def evaluate2(model, data_iter):
         prediction = torch.max(output_flat, 1)[1]
         total_correct += torch.sum((prediction == targets).float())
     return total_loss[0] / (total_count // batch_size), total_correct.data[0] / total_count # (ajusted_loss, acc)
+
+
+def train(model, epoch_number, cuda=True):
+    model.train()
+    total_loss = 0
+    total_pure_loss = 0  # without the penalization term
+    start_time = time.time()
+    step = 0
+
+    training_iter = data_iter(dataset.training_set, batch_size)
+    train_eval_iter = eval_iter(dataset.training_set[0:500], batch_size)
+    dev_iter = eval_iter(dataset.dev_set[0:500], batch_size)
+
+    training_loop(model, criterion, optimizer, training_iter, dev_iter, train_eval_iter, step, total_pure_loss, total_loss, cuda)
+
+    evaluate_start_time = time.time()
+    val_loss, acc = test(model, epoch_number, evaluate_start_time)
+    save_model(model, val_loss, acc, epoch_number)
+
 
 if __name__ == '__main__':
 
@@ -309,19 +242,19 @@ if __name__ == '__main__':
             'word-vector': "",
             'num_labels': 5,
             'cuda' : True,
-            'batch_size' : 20
+            'batch_size' : 20,
+            'useAttention':True
         }
     best_val_loss = None
     best_acc = None
 
     config = default_config
     # config["pooling"] = "all"
-    model = models.Classifier(config)
+    # model = models.Classifier(config)
+    model = models.LSTMSentiment(config)
+
     # WTF is this
     I = Variable(torch.ones(model.config['batch_size'], model.config['attention-hops'], model.config['attention-hops']))
-    # for i in range(batch_size):
-    #     for j in range(attention_hops):
-    #         I.data[i][j][j] = 1
     if cuda:
         model = model.cuda()
         I = I.cuda()
@@ -329,8 +262,6 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-
-    # importlib.reload(utils)
     for epoch in range(50):
-        train2(model, epoch)
+        train(model, epoch)
 
